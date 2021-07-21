5a2c7715aa79c8f7d06f348ee40e5cccbf81ed13
Red3ffect <red3ffect117@gmail.com>
2021-07-20 23:20:37 -0700

    Adds Post model and CRUD routes

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,113 @@
+const router = require('express').Router();
+//const sequelize = require('../../config/connection');
+const { User, Post } = require('../../models');
+
+// get ALL Users
+router.get('/', (req, res) => {
+    Post.findAll({
+        include: [
+            {
+                model: User,
+                attibutes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// get User by ID
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        // attributes: [
+        //     'id', 'title', 'post_body', 'created_at'
+        // ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No Post found with this id' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// create new Post
+router.post('/', (req, res) => {
+    Post.create({
+        title: req.body.title,
+        post_body: req.body.post_body,
+        user_id: req.body.user_id
+    })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// If likes and dislikes are added.... They go here :)
+
+// update Post by ID
+router.put('/:id', (req, res) => {
+    Post.update(
+        {
+            title: req.body.title,
+            post_body: req.body.post_body
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No Post found with this id' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.statsu(500).json(err);
+        });
+});
+
+// delete Post by ID
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No Post found with this id' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
