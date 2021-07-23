a0d7cec6604827c93500af402d0df72746ef3638
Red3ffect <red3ffect117@gmail.com>
2021-07-22 21:56:28 -0700

    Adds Homepage CRUD routes and handlebars layout, views, and partials

---
+++ controllers/home-routes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+// Render Homepage with all Post Data
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: ['id', 'title', 'post_body', 'created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+
+            res.render('homepage', {
+                posts
+                // loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
